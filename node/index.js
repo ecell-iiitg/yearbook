9f8d06da7b7610c93ccababf23be420911668cb6
Sameer Kumar Kushwaha <kushwahasameerkumar@gmail.com>
2020-08-05 15:43:18 +0530

    initial commit

---
+++ node/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+app.use("/", (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.use("/yearbook", express.static('res/Yearbook.pdf'))
+
+app.use("/", (req, res) => {
+    res.send("Server running...");
+});
+
+const PORT = 3000;
+server.listen(PORT, function () {
+    console.log("Server running on http://localhost:" + PORT);
+});
+
