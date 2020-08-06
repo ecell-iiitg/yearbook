ca01784cecd3b17e95c647b28a2e56cd00394a8a
Sameer Kumar Kushwaha <kushwahasameerkumar@gmail.com>
2020-08-06 19:06:23 +0530

    infinite plugin remove

--- res/pdf.js
+++ res/pdf.js
@@ -1,48 +1,52 @@
-const loadPage = pn => {
-    // If absolute URL from the remote server is provided, configure the CORS
-    // header on that server.
-    var url = 'https://kushwahasameerkumar.github.io/ecell-yearbook/res/yearbook/ecell_yearbook_'+pn+'.pdf';
-    // var url = 'http://localhost:3000/yearbook/ecell_yearbook_' + pn + '.pdf';
+const loadPage = async pn => {
+    return new Promise((resolve, reject) => {
+        // If absolute URL from the remote server is provided, configure the CORS
+        // header on that server.
+        var url = 'https://kushwahasameerkumar.github.io/ecell-yearbook/res/yearbook/ecell_yearbook_' + pn + '.pdf';
+        // var url = 'http://localhost:3000/yearbook/ecell_yearbook_' + pn + '.pdf';
 
-    // Loaded via <script> tag, create shortcut to access PDF.js exports.
-    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+        // Loaded via <script> tag, create shortcut to access PDF.js exports.
+        var pdfjsLib = window['pdfjs-dist/build/pdf'];
 
-    // The workerSrc property shall be specified.
-    pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.js';
+        // The workerSrc property shall be specified.
+        pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.js';
 
-    // Asynchronous download of PDF
-    var loadingTask = pdfjsLib.getDocument(url);
-    loadingTask.promise.then(function (pdf) {
-        console.log('PDF loaded');
+        // Asynchronous download of PDF
+        var loadingTask = pdfjsLib.getDocument(url);
+        loadingTask.promise.then(function (pdf) {
+            console.log('PDF loaded');
 
-        // Fetch the first page
-        var pageNumber = 1;
-        pdf.getPage(pageNumber).then(function (page) {
-            console.log('Page loaded');
+            // Fetch the first page
+            var pageNumber = 1;
+            pdf.getPage(pageNumber).then(function (page) {
+                console.log('Page loaded');
 
-            var scale = 1.5;
-            var viewport = page.getViewport({ scale: scale });
+                var scale = 1.5;
+                var viewport = page.getViewport({ scale: scale });
 
-            // Prepare canvas using PDF page dimensions
-            const canvasId = "pdf-canvas-" + pn;
-            var canvas = document.getElementById(canvasId);
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
+                // Prepare canvas using PDF page dimensions
+                const canvasId = "pdf-canvas-" + pn;
+                var canvas = document.getElementById(canvasId);
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
 
-            // Render PDF page into canvas context
-            var renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-            renderTask.promise.then(function () {
-                console.log('Page rendered');
+                // Render PDF page into canvas context
+                var renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                var renderTask = page.render(renderContext);
+                renderTask.promise.then(function () {
+                    console.log('Page rendered');
+                    resolve("Page rendered");
+                });
             });
+        }, function (reason) {
+            // PDF loading error
+                console.error(reason);
+                reject(reason);
         });
-    }, function (reason) {
-        // PDF loading error
-        console.error(reason);
     });
 };
 
@@ -55,16 +59,32 @@ const createPage = pageNumber => {
 };
 
 
-const appendPage = pageNumber => {
+const appendPage = async pageNumber => {
     createPage(pageNumber);
-    loadPage(pageNumber);
+    // showLoadingOnPage(pageNumber);
+    await loadPage(pageNumber);
+    return "Append completed";
 };
+
+const showLoadingOnPage = page => {
+    const pageCanvas = document.getElementById("ebook");
+    const loading = `<div id="loading-${page}" style="justify-content: center; align-content: center"> LOADING PAGE ${page}... </div>`;
+    pageCanvas.innerHTML += loading;
+};
+
+const hideLoadingOnPage = page => {
+    const loading = document.getElementById("loading-" + page);
+    if (loading) {
+        loading.parentNode.removeChild(loading);
+    }
+}
 
 // Endless scroll 
 $(window).scroll(function () {
     if ($(window).scrollTop() == $(document).height() - $(window).height()) {
         // run our call for pagination
-        appendPage(pages++);
+        const currentPage = pages++;
+        appendPage(currentPage);
     }
 });
 
